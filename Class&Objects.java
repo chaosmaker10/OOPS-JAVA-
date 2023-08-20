ba2358edcefb2c23927245f1887e4d51d575c434
chaosmaker10 <a.harikarthikeyan18@gmail.com>
2023-08-20 18:21:20 +0530

    3

--- Class&Objects.java
+++ Class&Objects.java
@@ -9,7 +9,7 @@
 
 // class is a blue print or a structure to a object
 import java.util.*;
-public class Applform{
+class Applform{
     
     String name;//By default it is default access modifier 
     int rollno;//By default it is default access modifier 
@@ -17,15 +17,23 @@ public class Applform{
     
     
     Applform(){
-        System.out.print("Constructor is being called");
+        System.out.println("Constructor is being called");
+    }
+    // This is method
+    // Function called inside the class is called as method 
+    // In java we have only methods no function exists
+    public void display(){  //This is function definition
+        System.out.println(name);
+        System.out.println(rollno);
     }
     
-    
     public static void main(String args[]){
-        Applform Hari=new Applform();
-        Hari.name="Hari";
-        Hari.rollno=75;   //In this line we are creating an object Hari which follows the blueprint Applform.
+        Applform Hari=new Applform();  //In this line we are creating an object Hari which follows the blueprint Applform.
         // The keyword new allocates new memory space in the ram whereas next one is a default construcutor;
-    
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        Hari.name="Hari";
+        Hari.rollno=75;
+        Hari.display();//This is function call 
     }
 }
